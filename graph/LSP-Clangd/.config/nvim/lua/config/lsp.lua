9bc08a506657e6130fe329650bb6766b05e5c465
Your Name <you@example.com>
2026-06-29 11:10:15 +0100

    cmake

--- graph/LSP-Clangd/.config/nvim/lua/config/lsp.lua
+++ graph/LSP-Clangd/.config/nvim/lua/config/lsp.lua
@@ -1,3 +1,8 @@
 vim.lsp.enable("lua_ls")
+
+-- # C++
 vim.lsp.enable('clangd')
 vim.lsp.enable('cmake')
+vim.lsp.enable('mesonlsp')
+-- Language server for Swift and C/C++/Objective-C.
+-- vim.lsp.enable('sourcekit')
